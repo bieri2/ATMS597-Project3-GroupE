60359a1d91e578290da73d89dd6cf1564f0c6715
jdthayer <60154140+jdthayer@users.noreply.github.com>
2020-02-25 14:23:07 -0600

    Now extracting Winds at Sig995 level + long-term mean data

--- Copy of ATMS597_Project3_Arka_ReanalysisCell.py
+++ Copy of ATMS597_Project3_Arka_ReanalysisCell.py
@@ -12,8 +12,8 @@ datasets_Uwind_850hPa = []
 datasets_Vwind_850hPa = []
 datasets_SpecHum_850hPa = []
 datasets_AirTemp_850hPa = []
-datasets_Uwind_10m = []
-datasets_Vwind_10m = []
+datasets_Uwind_sig995 = []
+datasets_Vwind_sig995 = []
 datasets_SkinTemp_Sfc = []
 datasets_PrecipWater = []
 
@@ -45,9 +45,9 @@ for iyr in years:
                             level=850, time=dates_year)
       ds_AirTemp_850hPa = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.dailyavgs/pressure/air.'+str(iyr)+'.nc',engine='netcdf4').sel(\
                             level=850, time=dates_year)
-      ds_Uwind_10m = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.dailyavgs/surface_gauss/uwnd.10m.gauss.'+str(iyr)+'.nc',engine='netcdf4').sel(\
+      ds_Uwind_sig995 = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.dailyavgs/surface/uwnd.sig995.gauss.'+str(iyr)+'.nc',engine='netcdf4').sel(\
                             time=dates_year)
-      ds_Vwind_10m = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.dailyavgs/surface_gauss/vwnd.10m.gauss.'+str(iyr)+'.nc',engine='netcdf4').sel(\
+      ds_Vwind_sig995 = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.dailyavgs/surface/vwnd.sig995.gauss.'+str(iyr)+'.nc',engine='netcdf4').sel(\
                             time=dates_year)
       ds_SkinTemp_Sfc = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.dailyavgs/surface_gauss/skt.sfc.gauss.'+str(iyr)+'.nc',engine='netcdf4').sel(\
                             time=dates_year)
@@ -65,8 +65,8 @@ for iyr in years:
       datasets_Vwind_850hPa.append(ds_Vwind_850hPa)
       datasets_SpecHum_850hPa.append(ds_SpecHum_850hPa)
       datasets_AirTemp_850hPa.append(ds_AirTemp_850hPa)
-      datasets_Uwind_10m.append(ds_Uwind_10m)
-      datasets_Vwind_10m.append(ds_Vwind_10m)
+      datasets_Uwind_sig995.append(ds_Uwind_sig995)
+      datasets_Vwind_sig995.append(ds_Vwind_sig995)
       datasets_SkinTemp_Sfc.append(ds_SkinTemp_Sfc)
       datasets_PrecipWater.append(ds_PrecipWater)
 
@@ -82,8 +82,8 @@ combined_Uwind_850hPa = xr.concat(datasets_Uwind_850hPa, dim='time')
 combined_Vwind_850hPa = xr.concat(datasets_Vwind_850hPa, dim='time')
 combined_SpecHum_850hPa = xr.concat(datasets_SpecHum_850hPa, dim='time')
 combined_AirTemp_850hPa = xr.concat(datasets_AirTemp_850hPa, dim='time')
-combined_Uwind_10m = xr.concat(datasets_Uwind_10m, dim='time')
-combined_Vwind_10m = xr.concat(datasets_Vwind_10m, dim='time')
+combined_Uwind_sig995 = xr.concat(datasets_Uwind_sig995, dim='time')
+combined_Vwind_sig995 = xr.concat(datasets_Vwind_sig995, dim='time')
 combined_SkinTemp_Sfc = xr.concat(datasets_SkinTemp_Sfc, dim='time')
 combined_PrecipWater = xr.concat(datasets_PrecipWater, dim='time')
 
@@ -98,10 +98,47 @@ combined_Uwind_850hPa.to_netcdf('combined_Uwind_850hPa_1996to2019_ExtremePrecipD
 combined_Vwind_850hPa.to_netcdf('combined_Vwind_850hPa_1996to2019_ExtremePrecipDays.nc')
 combined_SpecHum_850hPa.to_netcdf('combined_SpecHum_850hPa_1996to2019_ExtremePrecipDays.nc')
 combined_AirTemp_850hPa.to_netcdf('combined_AirTemp_850hPa_1996to2019_ExtremePrecipDays.nc')
-combined_Uwind_10m.to_netcdf('combined_Uwind_10m_1996to2019_ExtremePrecipDays.nc')
-combined_Vwind_10m.to_netcdf('combined_Vwind_10m_1996to2019_ExtremePrecipDays.nc')
+combined_Uwind_sig995.to_netcdf('combined_Uwind_sig995_1996to2019_ExtremePrecipDays.nc')
+combined_Vwind_sig995.to_netcdf('combined_Vwind_sig995_1996to2019_ExtremePrecipDays.nc')
 combined_SkinTemp_Sfc.to_netcdf('combined_SkinTemp_Sfc_1996to2019_ExtremePrecipDays.nc')
 combined_PrecipWater.to_netcdf('combined_PrecipWater_1996to2019_ExtremePrecipDays.nc')
 
 # Move newly-created Netcdf files into your Google Drive 
 !mv *.nc "/content/drive/My Drive/"
+
+##############################################################################################################################
+
+## Importing Long-term Mean NCEP Reanalysis data from 1981-2010, only selecting DJF
+
+months = xr.cftime_range(start='0001-01-01', end='0001-12-01', freq='MS', calendar = 'standard') # selecting long-term mean data for 1981-2010 with cftime.DatetimeGregorian format
+months = months[(months.month==12)|(months.month==1)|(months.month==2)] # selecting long-term mean data for DJF in 1981-2010
+
+# Extract data
+ds_Uwind_250hPa_LTM = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/pressure/uwnd.mon.1981-2010.ltm.nc',engine='netcdf4').sel(\
+                      level=250, time = months)
+ds_Vwind_250hPa_LTM = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/pressure/vwnd.mon.1981-2010.ltm.nc',engine='netcdf4').sel(\
+                      level=250, time = months)
+ds_GeopHgt_500hPa_LTM = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/pressure/hgt.mon.1981-2010.ltm.nc',engine='netcdf4').sel(\
+                      level=500, time = months)
+ds_Uwind_500hPa_LTM = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/pressure/uwnd.mon.1981-2010.ltm.nc',engine='netcdf4').sel(\
+                      level=500, time = months)
+ds_Vwind_500hPa_LTM = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/pressure/vwnd.mon.1981-2010.ltm.nc',engine='netcdf4').sel(\
+                      level=500, time = months)
+ds_Omega_500hPa_LTM = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/pressure/omega.mon.1981-2010.ltm.nc',engine='netcdf4').sel(\
+                      level=500, time = months)
+ds_Uwind_850hPa_LTM = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/pressure/uwnd.mon.1981-2010.ltm.nc',engine='netcdf4').sel(\
+                      level=850, time = months)
+ds_Vwind_850hPa_LTM = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/pressure/vwnd.mon.1981-2010.ltm.nc',engine='netcdf4').sel(\
+                      level=850, time = months)
+ds_SpecHum_850hPa_LTM = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/pressure/shum.mon.1981-2010.ltm.nc',engine='netcdf4').sel(\
+                      level=850, time = months)
+ds_AirTemp_850hPa_LTM = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/pressure/air.mon.1981-2010.ltm.nc',engine='netcdf4').sel(\
+                      level=850, time = months)
+ds_Uwind_sig995_LTM = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/surface/uwnd.sig995.mon.1981-2010.ltm.nc',engine='netcdf4').sel(\
+                      time = months)
+ds_Vwind_sig995_LTM = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/surface/vwnd.sig995.mon.1981-2010.ltm.nc',engine='netcdf4').sel(\
+                      time = months)
+ds_SkinTemp_Sfc_LTM = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/surface_gauss/skt.sfc.mon.1981-2010.ltm.nc',engine='netcdf4').sel(\
+                      time = months)
+ds_PrecipWater_LTM = xr.open_dataset('https://www.esrl.noaa.gov/psd/thredds/dodsC/Datasets/ncep.reanalysis.derived/surface/pr_wtr.eatm.mon.1981-2010.ltm.nc',engine='netcdf4').sel(\
+                      time = months)
